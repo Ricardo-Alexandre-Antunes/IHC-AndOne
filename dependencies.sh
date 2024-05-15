8a3debc74399e99bbd8cedafd55e761c8e90391f
ricardo.alexandre.antunes@ua.pt <ricardo.alexandre.antunes@ua.pt>
2024-05-15 22:56:09 +0100

    goddamn

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,4 @@
+npm install
 npm i react-router-dom
 npm install react-bootstrap bootstrap
 npm install bootstrap
@@ -6,7 +7,5 @@ npm i --save-dev sass
 npm install --save @fortawesome/fontawesome-svg-core
 npm install --save @fortawesome/free-solid-svg-icons
 npm install --save @fortawesome/react-fontawesome
-npm install react-bootstrap-range-slider
-npm install rc-slider
 npm install jspdf
 npm install jspdf-autotable

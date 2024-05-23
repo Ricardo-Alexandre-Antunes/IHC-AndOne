0a673ed897bc92b8d12ea34ccdc2e25c02a2e12d
ricardo.alexandre.antunes@ua.pt <ricardo.alexandre.antunes@ua.pt>
2024-05-23 17:45:23 +0100

    wtf

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  build: {
-    rollupOptions: {
-      external: ['/src/components/HomeCarousel' , '/src/components/MyFooter' , '/src/components/MyNavbar' , '/src/components/Product' , '/src/components/AcessoriosSearch' , '/src/components/Login', '/src/components/EquipamentosSearch']
-    }
-  }
-}
-
+export default defineConfig({
+  plugins: [react()],
+})
